96ebd4f3b3af6f12c729b20ef2c260d6f2214010
Yures1lva <yure.siilva9@gmail.com>
2024-03-19 17:33:04 -0300

    pipes e binding 14 e 15

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrl: './pipes.component.scss'
+})
+export class PipesComponent {
+  text: string = "Texto como propriedade"
+
+  today = new Date()
+}
